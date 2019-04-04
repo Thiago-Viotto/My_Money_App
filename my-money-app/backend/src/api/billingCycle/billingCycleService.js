47008dee6654a83d66d157c669afda023bba9b95
Thiago Viotto <thiagohenriqueviotto1@gmail.com>
2019-04-03 21:42:36 -0300

    Returning count and summary in billingCycle

--- my-money-app/backend/src/api/billingCycle/billingCycleService.js
+++ my-money-app/backend/src/api/billingCycle/billingCycleService.js
@@ -7,4 +7,41 @@ BillingCycle.methods(['get', 'post', 'put', 'delete'])
 // valida o retorno dos registros atualizados
 BillingCycle.updateOptions({ new: true, runValidators: true })
 
+// Uma nova rota chamada count com um middleware
+BillingCycle.route('count', (req, res, next) => {
+    // o .count já retorna a count de registros  
+    BillingCycle.count((error, value) => {
+        // garante o tratamento de um erro no frontend
+        // o frontend receberá um array com os erros
+        if (error) {
+            res.status(500).json({ error: [error] })
+        } else {
+            // retorna se não der erro a count de registros
+            res.json({ value })
+        }
+    })
+})
+
+BillingCycle.route('summary', (req, res, next) => {
+    // o $project extrai um campo do json
+    BillingCycle.aggregate([{
+        // credit e debt recebem a soma dos valores dos créditos e débitos que estão no JSON
+        $project: { credit: { $sum: "$credits.value" }, debt: { $sum: "$debts.value" } }
+    }, {
+        // o $group agrupa os valores (group by)
+        // o credit recebe a soma de todos os créditos que saíram do $project
+        $group: { _id: null, credit: { $sum: "$credit" }, debt: { $sum: "$debt" } }
+    }, {
+        // projeta os elementos que irão aparecer (0 - false, 1 - true)
+        $project: { _id: 0, credit: 1, debt: 1 }
+    }]).exec((error, result) => {
+        if (error) {
+            res.status(500).json({ errors: [error] })
+        } else {
+            res.json(result[0] || { credit: 0, debt: 0 })
+        }
+    })
+})
+
 module.exports = BillingCycle
+
