e6136b714e87e7e0def1693da103dfd98ff53158
Thiago Viotto <thiagohenriqueviotto1@gmail.com>
2019-05-03 19:50:54 -0300

    Added template header

---
+++ my-money-app/frontend/src/main/App.jsx
@@ -0,0 +1,11 @@
+import '../common/template/dependences'
+import Header from '../common/template/header'
+
+import React from 'react'
+
+export default props => (
+    <div className="wrapper">
+        <Header></Header>
+
+    </div>
+)
