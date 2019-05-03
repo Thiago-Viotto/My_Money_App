7ec4a2fc8694b94f6a054792401dbbb6eb796f34
Thiago Viotto <thiagohenriqueviotto1@gmail.com>
2019-05-03 20:10:22 -0300

    Added template Menu

--- my-money-app/frontend/src/main/App.jsx
+++ my-money-app/frontend/src/main/App.jsx
@@ -1,11 +1,13 @@
 import '../common/template/dependences'
 import Header from '../common/template/header'
+import SideBar from '../common/template/sideBar'
 
 import React from 'react'
 
 export default props => (
     <div className="wrapper">
-        <Header></Header>
+        <Header />
+        <SideBar />
 
     </div>
 )
