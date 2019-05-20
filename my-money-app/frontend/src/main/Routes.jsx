b43c4f7a1d52a292188e67c65982fbfe1a3e190c
Thiago Viotto <thiagohenriqueviotto1@gmail.com>
2019-05-20 20:28:01 -0300

    Dashboard without redux

--- my-money-app/frontend/src/main/Routes.jsx
+++ my-money-app/frontend/src/main/Routes.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { Router, Route, Redirect, hashHistory } from 'react-router'
 
-import Dashboard from '../Dashboard/dashboard'
+// import Dashboard from '../Dashboard2/dashboard2' //without Redux 
+import Dashboard from '../Dashboard/dashboard' //with Redux 
 import BillingCycle from '../billingCycle/billingCycle'
 
 export default props => (
