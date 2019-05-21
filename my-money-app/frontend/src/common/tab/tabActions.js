3954527671f166f8959071cb7232534b6fbaa547
Thiago Viotto <thiagohenriqueviotto1@gmail.com>
2019-05-20 21:23:53 -0300

    reducer tabReducer

---
+++ my-money-app/frontend/src/common/tab/tabActions.js
@@ -0,0 +1,7 @@
+export function selectTab(tabId){
+    
+    return {
+        type: 'TAB_SELECTED',
+        payload: tabId
+    }
+}
--- my-money-app/frontend/src/main/reducers.js
+++ my-money-app/frontend/src/main/reducers.js
@@ -1,9 +1,11 @@
 import {combineReducers} from 'redux'
 
 import DashboardReducer from '../Dashboard/dashboardReducer'
+import TabReducer from '../common/tab/tabReducer'
 
 const rootReducer = combineReducers({
-    dashboard: DashboardReducer 
+    dashboard: DashboardReducer,
+    tab: TabReducer
 
 })
 
