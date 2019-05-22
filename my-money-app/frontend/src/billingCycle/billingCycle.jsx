d243827758f814ccc2837ea7e47bc473be600132
Thiago Viotto <thiagohenriqueviotto1@gmail.com>
2019-05-22 20:25:20 -0300

    hiding tabs

--- my-money-app/frontend/src/billingCycle/billingCycle.jsx
+++ my-money-app/frontend/src/billingCycle/billingCycle.jsx
@@ -9,11 +9,12 @@ import TabsHeader from '../common/tab/tabsHeader'
 import TabsContent from '../common/tab/tabsContent'
 import TabHeader from '../common/tab/tabHeader'
 import TabContent from '../common/tab/TabContent'
-import {selectTab} from '../common/tab/tabActions'
+import {selectTab, showTabs} from '../common/tab/tabActions'
 
 class BillyngCycle extends Component {
     componentWillMount(){
         this.props.selectTab('tabList')
+        this.props.showTabs('tabList', 'tabCreate')
     }
 
     render() {
@@ -41,5 +42,5 @@ class BillyngCycle extends Component {
     }
 }
 
-const mapDispatchToProps = dispatch => bindActionCreators({selectTab}, dispatch)
+const mapDispatchToProps = dispatch => bindActionCreators({selectTab, showTabs}, dispatch)
 export default connect(null, mapDispatchToProps)(BillyngCycle)
--- my-money-app/frontend/src/common/tab/tabActions.js
+++ my-money-app/frontend/src/common/tab/tabActions.js
@@ -1,7 +1,16 @@
-export function selectTab(tabId){
-    
+export function selectTab(tabId) {
+
     return {
         type: 'TAB_SELECTED',
         payload: tabId
+    }
+}
+
+export function showTabs(...tabIds) {
+    const tabsToShow = {}
+    tabIds.forEach(e => tabsToShow[e] = true)   //aba que eu quero exibir
+    return {
+        type: 'TAB_SHOWED',
+        payload: tabsToShow
     }
 }
