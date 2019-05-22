91104638fb0121fde699a0800c843783d0ebd5ad
Thiago Viotto <thiagohenriqueviotto1@gmail.com>
2019-05-22 19:41:10 -0300

    bindActionCreator and connect

--- my-money-app/frontend/src/common/tab/tabHeader.jsx
+++ my-money-app/frontend/src/common/tab/tabHeader.jsx
@@ -1,17 +1,26 @@
-import React,{Component} from 'react'
+import React, { Component } from 'react'
+import { bindActionCreator } from 'redux'
+import { connect } from 'react-redux'
 
-class TabHeader extends Component{
-    render(){
+import { selectedTab } from './tabActions'
+
+class TabHeader extends Component {
+    render() {
+        const selected = this.props.tab.selected === this.props.target
         return (
-            <li>
-                <a href='javascript:;' 
+            <li className={selected ? 'active' : ''}>
+                <a href='javascript:;'
                     data-toggle='tab'
+                    onClick = {() => this.props.selectedTab(this.props.target)}
                     data-target={this.props.target}>
                     <i className={`fa fa-${this.props.icon}`}></i>
                     {this.props.label}
-                    </a>
+                </a>
             </li>
         )
     }
 }
-export default TabHeader
+
+const mapStateToProps = state => ({ tab: state.tab })
+const mapDispatchToProps = dispatch => bindActionCreator({selectedTab}, dispatch)
+export default connect(mapStateToProps, mapDispatchToProps) (TabHeader)
