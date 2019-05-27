0ad31a3eb91c6b492e152ed46f85748f497a9681
Thiago Viotto <thiagohenriqueviotto1@gmail.com>
2019-05-27 11:23:33 -0300

    Created actionCreator and reducer of component BillingCycleList

--- my-money-app/frontend/src/main/reducers.js
+++ my-money-app/frontend/src/main/reducers.js
@@ -2,10 +2,12 @@ import {combineReducers} from 'redux'
 
 import DashboardReducer from '../Dashboard/dashboardReducer'
 import TabReducer from '../common/tab/tabReducer'
+import BillingCyclesReducer from '../billingCycle/billingCycleReducer'
 
 const rootReducer = combineReducers({
     dashboard: DashboardReducer,
-    tab: TabReducer
+    tab: TabReducer,
+    billingCycle: BillingCyclesReducer
 
 })
 
