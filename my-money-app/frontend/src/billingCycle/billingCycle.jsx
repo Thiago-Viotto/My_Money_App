58df63daf3cbebe7446ae010ef90e506f30dd9f6
Thiago Viotto <thiagohenriqueviotto1@gmail.com>
2019-06-12 17:48:07 -0300

    Added formValueSelector in billingCycleForm

--- my-money-app/frontend/src/billingCycle/billingCycle.jsx
+++ my-money-app/frontend/src/billingCycle/billingCycle.jsx
@@ -9,15 +9,13 @@ import TabsHeader from '../common/tab/tabsHeader'
 import TabsContent from '../common/tab/tabsContent'
 import TabHeader from '../common/tab/tabHeader'
 import TabContent from '../common/tab/TabContent'
-import { selectTab, showTabs } from '../common/tab/tabActions'
-import { create, update, remove } from './billingCycleActions'
+import {init, create, update, remove } from './billingCycleActions'
 import List from './billingCycleList'
 import Form from './billingCycleForm'
 
 class BillyngCycle extends Component {
     componentWillMount() {
-        this.props.selectTab('tabList')
-        this.props.showTabs('tabList', 'tabCreate')
+        this.props.init()
     }
 
     render() {
@@ -56,5 +54,5 @@ class BillyngCycle extends Component {
     }
 }
 
-const mapDispatchToProps = dispatch => bindActionCreators({ selectTab, showTabs, create, update, remove }, dispatch)
+const mapDispatchToProps = dispatch => bindActionCreators({ init, create, update, remove }, dispatch)
 export default connect(null, mapDispatchToProps)(BillyngCycle)
--- my-money-app/frontend/src/billingCycle/billingCycleForm.jsx
+++ my-money-app/frontend/src/billingCycle/billingCycleForm.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { reduxForm, Field } from 'redux-form'
+import { reduxForm, Field, formValueSelector } from 'redux-form'
 
 import labelAndInput from '../common/form/labelAndInput'
 import { init } from './billingCycleActions'
@@ -9,7 +9,7 @@ import CreditList from './creditList'
 
 class BillingCycleForm extends Component {
     render() {
-        const { handleSubmit, readOnly } = this.props
+        const { handleSubmit, readOnly, credits } = this.props
 
         return (
             <form role='form' onSubmit={handleSubmit}>
@@ -17,10 +17,10 @@ class BillingCycleForm extends Component {
                     <Field name='name' component={labelAndInput} readOnly={readOnly}
                         label='Nome' cols='12 4' placeholder='Informe o nome' />
                     <Field name='month' component={labelAndInput} type='number' readOnly={readOnly}
-                        label='Mês' cols='12 4' placeholder='informe o mês'  />
+                        label='Mês' cols='12 4' placeholder='informe o mês' />
                     <Field name='year' component={labelAndInput} type='number' readOnly={readOnly}
                         label='Ano' cols='12 4' placeholder='Informe o ano' />
-                    <CreditList cols='12 6' readOnly={readOnly} />
+                    <CreditList cols='12 6' list={credits} readOnly={readOnly} />
                 </div>
                 <div className='box-footer'>
                     <button type='submit' className={`btn btn-${this.props.submitClass}`}>{this.props.submitLabel}</button>
@@ -33,6 +33,8 @@ class BillingCycleForm extends Component {
 }
 
 BillingCycleForm = reduxForm({ form: 'billingCycleForm', destroyOnUnmount: false })(BillingCycleForm)
+const selector = formValueSelector('billingCycleForm') //extraindo dado do array
+const mapStateToProps = state => ({ credits: selector(state, 'credits') })
 const mapDispatchToProps = dispatch => bindActionCreators({ init }, dispatch)
 
-export default connect(null, mapDispatchToProps)(BillingCycleForm)
+export default connect(mapStateToProps, mapDispatchToProps)(BillingCycleForm)
