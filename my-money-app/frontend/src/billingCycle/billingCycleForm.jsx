0a82de781622616319a3e44fb4af9c1ce42fd491
Thiago Viotto <thiagohenriqueviotto1@gmail.com>
2019-06-16 11:58:45 -0300

    Added component summary

--- my-money-app/frontend/src/billingCycle/billingCycleForm.jsx
+++ my-money-app/frontend/src/billingCycle/billingCycleForm.jsx
@@ -6,11 +6,21 @@ import labelAndInput from '../common/form/labelAndInput'
 import { init } from './billingCycleActions'
 import { bindActionCreators } from 'redux'
 import ItemList from './itemList'
+import Summary from './summary'
 
 class BillingCycleForm extends Component {
+
+    calculateSummary() {
+        const sum = (t, v) => t + v
+        return {
+            sumOfCredits: this.props.credits.map(c => +c.value || 0).reduce(sum),
+            sumOfDebts: this.props.debts.map(d => +d.value || 0).reduce(sum)
+        }
+    }
+
     render() {
         const { handleSubmit, readOnly, credits, debts } = this.props
-
+        const { sumOfCredits, sumOfDebts } = this.calculateSummary()
         return (
             <form role='form' onSubmit={handleSubmit}>
                 <div className='box-body'>
@@ -20,6 +30,7 @@ class BillingCycleForm extends Component {
                         label='Mês' cols='12 4' placeholder='informe o mês' />
                     <Field name='year' component={labelAndInput} type='number' readOnly={readOnly}
                         label='Ano' cols='12 4' placeholder='Informe o ano' />
+                    <Summary credit={sumOfCredits} debt={sumOfDebts} />
                     <ItemList cols='12 6' list={credits} readOnly={readOnly}
                         field='credits' legend='Créditos' />
                     <ItemList cols='12 6' list={debts} readOnly={readOnly}
