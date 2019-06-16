f153ea5eb2a44de5d26737a170d5d529e03e79f3
Thiago Viotto <thiagohenriqueviotto1@gmail.com>
2019-06-16 11:23:21 -0300

    refatoring creditList to itemList

--- my-money-app/frontend/src/billingCycle/billingCycleForm.jsx
+++ my-money-app/frontend/src/billingCycle/billingCycleForm.jsx
@@ -5,7 +5,7 @@ import { reduxForm, Field, formValueSelector } from 'redux-form'
 import labelAndInput from '../common/form/labelAndInput'
 import { init } from './billingCycleActions'
 import { bindActionCreators } from 'redux'
-import CreditList from './creditList'
+import ItemList from './itemList'
 
 class BillingCycleForm extends Component {
     render() {
@@ -20,7 +20,8 @@ class BillingCycleForm extends Component {
                         label='Mês' cols='12 4' placeholder='informe o mês' />
                     <Field name='year' component={labelAndInput} type='number' readOnly={readOnly}
                         label='Ano' cols='12 4' placeholder='Informe o ano' />
-                    <CreditList cols='12 6' list={credits} readOnly={readOnly} />
+                    <ItemList cols='12 6' list={credits} readOnly={readOnly} 
+                    field='credits' legend='Créditos' />
                 </div>
                 <div className='box-footer'>
                     <button type='submit' className={`btn btn-${this.props.submitClass}`}>{this.props.submitLabel}</button>
--- my-money-app/frontend/src/billingCycle/itemList.jsx
+++ my-money-app/frontend/src/billingCycle/itemList.jsx
@@ -5,17 +5,17 @@ import Grid from '../common/layout/Grid'
 import { Field, arrayInsert, arrayRemove } from 'redux-form'
 import Input from '../common/input'
 
-class CreditList extends Component {
+class ItemList extends Component {
 
     add(index, item = {}){
         if(!this.props.readOnly){
-            this.props.arrayInsert('billingCycleForm', 'credits', index, item)
+            this.props.arrayInsert('billingCycleForm', this.props.field, index, item)
         }
     }
 
     remove(index){
         if(!this.props.readOnly && this.props.list.length > 1){
-            this.props.arrayRemove('billingCycleForm', 'credits', index)
+            this.props.arrayRemove('billingCycleForm', this.props.field, index)
         }
     }
 
@@ -23,9 +23,9 @@ class CreditList extends Component {
         const list = this.props.list || []
         return list.map((item, index) => (
             <tr key={index}>
-                <td><Field name={`credits[${index}].name`} component={Input}
+                <td><Field name={`${this.props.field}[${index}].name`} component={Input}
                     placeholder='Informe o nome' readOnly={this.props.readOnly} /> </td>
-                <td><Field name={`credits[${index}].value`} component={Input}
+                <td><Field name={`${this.props.field}[${index}].value`} component={Input}
                     placeholder='Informe o valor' readOnly={this.props.readOnly} /> </td>
                 <td>
                     <button type='button' className='btn btn-success'
@@ -49,7 +49,7 @@ class CreditList extends Component {
         return (
             <Grid cols={this.props.cols}>
                 <fieldset>
-                    <legend>Créditos</legend>
+                    <legend>{this.props.legend}</legend>
                     <table className='table'>
                         <tr>
                             <th>Nome</th>
@@ -68,4 +68,4 @@ class CreditList extends Component {
 }
 
 const mapDispatchToProps = dispatch => bindActionCreators({arrayInsert, arrayRemove}, dispatch)
-export default connect(null,mapDispatchToProps) (CreditList)
+export default connect(null,mapDispatchToProps) (ItemList)
